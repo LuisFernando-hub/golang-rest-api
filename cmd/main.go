8330e97b9fb020cd2a16cb75d1a4d40313babe40
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2025-11-19 16:08:04 +0400

    creating connection with mysql database

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"ecom/cmd/api"
+	"ecom/config"
+	"ecom/db"
 	"log"
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// Application entry point
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Net: "tcp",
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		ParseTime: true,
+		AllowNativePasswords: true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	log.Println("Connected to database")
 }
